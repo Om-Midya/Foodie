a23e7f0d0f39a4fc8c7a0e7db8ea025161d708bd
Om-Midya <dukemidya7521@gmail.com>
2024-06-23 20:29:04 +0530

    Created an admin side view and made necessary bug-fixes in navigation

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,128 @@
+import {View, Text, Image, StyleSheet, Pressable, Button} from "react-native";
+import {Stack, useLocalSearchParams, useRouter} from "expo-router";
+import products from "@/assets/data/products";
+import {useState} from "react";
+import {circleAsync} from "@expo/image-utils/build/jimp";
+import {PizzaSize} from "@/src/types";
+import {useCart} from "@/src/providers/CartProvider";
+
+const ProductDetailsScreen = () => {
+    const {id} = useLocalSearchParams()
+    const {addItem} = useCart();
+    const sizes: PizzaSize[]= ['S', 'M', 'L', 'XL'];
+
+    const [selectedSize, setSelectedSize] = useState<PizzaSize>('M');
+
+    const router = useRouter();
+
+    const product = products.find((product) => product.id.toString() === id);
+
+    const addToCart = () => {
+        if(product) {
+            addItem(product, selectedSize, price(product))
+            router.navigate('/cart');
+        }
+        else {
+            return <Text>Product Not found</Text>
+        }
+    }
+
+    const price = (product: typeof products[0] | undefined) => {
+        if(!product) return '0';
+        switch(selectedSize) {
+            case 'S':
+                return (product.price * 0.8).toFixed(2);
+            case 'M':
+                return product.price.toFixed(2);
+            case 'L':
+                return (product.price * 1.5).toFixed(2);
+            case 'XL':
+                return (product.price * 1.75).toFixed(2);
+            default:
+                return product.price.toFixed(2);
+        }
+    }
+
+    //console.log(price(product));
+
+    return (
+        <View style={styles.container}>
+            {/* Using the Stack Screen here gives the advantage of using various props associated to the product */}
+            <Stack.Screen options={{title: `${product?.name}`}}/>
+
+            <Image source={{uri: product?.image}} style={styles.image}/>
+            {/*<Text style={{fontSize:20}}>Available Sizes:</Text>*/}
+            {/*<View style={styles.sizes}>*/}
+            {/*    {sizes.map((size) => (*/}
+            {/*        <Pressable*/}
+            {/*            key={size}*/}
+            {/*            style={size===selectedSize? styles.selectedSize:styles.size}*/}
+            {/*            onPress={() => setSelectedSize(size as PizzaSize)}*/}
+            {/*        >*/}
+            {/*            <Text*/}
+            {/*                key={size}*/}
+            {/*                style={size===selectedSize? styles.selectedSizeText:styles.sizeText}*/}
+            {/*            >{size}</Text>*/}
+            {/*        </Pressable>*/}
+            {/*    ))}*/}
+            {/*</View>*/}
+
+            <View style={styles.price}>
+                <Text style={styles.priceText}>Default Price(M): ${product?.price}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+        resizeMode: 'cover',
+    },
+    sizes: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        margin: 10,
+    },
+    size: {
+        width: 70,
+        height: 70,
+        backgroundColor: 'lightgrey',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 35,
+        margin: 5,
+    },
+    selectedSize: {
+        width: 70,
+        height: 70,
+        backgroundColor: '#2f95dc',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 35,
+        margin: 5,
+    },
+    sizeText: {
+        fontSize: 25,
+    },
+    selectedSizeText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    price: {
+        marginTop: 10,
+    },
+    priceText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    }
+})
+
+export default ProductDetailsScreen;
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -3,7 +3,7 @@ import {Text} from "@/src/components/Themed";
 import Colors from "@/src/constants/Colors";
 import {StyleSheet} from "react-native";
 import {Product} from "@/src/types.ts"
-import {Link} from "expo-router";
+import {Link, useSegments} from "expo-router";
 
 
 export const defaultPizzaImage = 'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg';
@@ -13,8 +13,10 @@ type ProductItemProps = {
 }
 
 const ProductItem = ({product}: ProductItemProps) => {
+    const segments = useSegments();
+    console.log(segments);
     return (
-        <Link href={`/menu/${product.id}`} asChild>
+        <Link href={`/${segments[0]}/menu/${product.id}`} asChild>
             <Pressable style={styles.container}>
                 <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} />
                 <Text style={styles.title}> {product.name} </Text>
